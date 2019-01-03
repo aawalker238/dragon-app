d7c30e36c6c7cc671ed5512bf9cccddd4122ca81
Anthony Walker <anthonyaustinwalker@gmail.com>
2019-01-03 13:08:25 -0800

    adding dragon class

--- backend/dragon.js
+++ backend/dragon.js
@@ -1,3 +1,30 @@
-class Dragon {
-  constructor() {}
+const TRAITS = require("./traits.json");
+
+const DEFAULT_PROPERTIES = {
+  nickname: "unnamed",
+  get birthdate() {
+    return new Date();
+  },
+  // * random traits in case of absence of value
+  get traits() {
+    const traits = [];
+
+    TRAITS.forEach((TRAIT) => {
+      const traitType = TRAIT.type;
+      const traitValues = TRAIT.values;
+      const traitValue = traitValues[Math.floor(Math.random() * traitValues.length)];
+      traits.push({traitType, traitValue});
+    })
+    return traits;
+  }
 }
+
+class Dragon {
+  constructor({birthdate, nickname, traits} = {}) {
+    this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
+    this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+    this.traits = traits || DEFAULT_PROPERTIES.traits;
+  }
+}
+
+module.exports = Dragon;
